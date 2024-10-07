e55cab29e2b86c794a26fcafd528a01f6458722b
IvaNeighbor <duzak2006@gmail.com>
2024-10-07 13:47:12 +0300

    start

--- TeamProject.cpp
+++ TeamProject.cpp
@@ -1,11 +1,50 @@
 ﻿#include <iostream>
-
 using namespace std;
 
-int main()
-{
-    cout << "Hello Everyone!\n";
-    cout << "Nice to meet you!\n";
-    cout << "Have a nice day\n";
-    cout << "Let`s start\n";
+void countingSort(int arr[], int size) {
+    // Знаходимо максимальний елемент
+    int maxElement = arr[0];
+    for (int i = 1; i < size; i++) {
+        if (arr[i] > maxElement) {
+            maxElement = arr[i];
+        }
+    }
+
+    // Створюємо масив для підрахунку елементів
+    int count[maxElement + 1] = { 0 };  // Ініціалізуємо масив з нулями
+
+    // Підраховуємо кількість кожного елемента
+    for (int i = 0; i < size; i++) {
+        count[arr[i]]++;
+    }
+
+    // Записуємо відсортовані елементи назад в початковий масив
+    int index = 0;
+    for (int i = 0; i <= maxElement; i++) {
+        while (count[i] > 0) {
+            arr[index++] = i;
+            count[i]--;
+        }
+    }
+}
+
+int main() {
+    int arr[] = { 4, 2, 2, 8, 3, 3, 1 };
+    int size = sizeof(arr) / sizeof(arr[0]);  // Розмір масиву
+
+    cout << "Початковий масив: ";
+    for (int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    countingSort(arr, size);  // Викликаємо функцію сортування
+
+    cout << "Відсортований масив: ";
+    for (int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
 }
