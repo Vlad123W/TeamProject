bc7c16f06a0d9eeb677b33dc97ff1b93584a6481
Admin <126456347+Vlad123W@users.noreply.github.com>
2024-10-07 14:05:52 +0300

    .

--- TeamProject.cpp
+++ TeamProject.cpp
@@ -1,10 +1,39 @@
 ﻿#include <iostream>
+
 using namespace std;
 
-void countingSort(int arr[], int size) {
+void countingSort(int arr[], int size);
+
+int main() 
+{
+    int arr[] = { 4, 2, 2, 8, 3, 3, 1 };
+    int size = sizeof(arr) / sizeof(arr[0]);  // Розмір масиву
+
+    cout << "Array: ";
+    for (int i = 0; i < size; i++) 
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    countingSort(arr, size);  // Викликаємо функцію сортування
+
+    cout << "Sorted array: ";
+    for (int i = 0; i < size; i++) 
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
+
+void countingSort(int arr[], int size) 
+{
     // Знаходимо максимальний елемент
     int maxElement = arr[0];
-    for (int i = 1; i < size; i++) {
+    for (int i = 1; i < size; i++) 
+    {
         if (arr[i] > maxElement) {
             maxElement = arr[i];
         }
@@ -14,14 +43,17 @@ void countingSort(int arr[], int size) {
     int* count = new int[maxElement + 1]();  // Ініціалізуємо масив з нулями
 
     // Підраховуємо кількість кожного елемента
-    for (int i = 0; i < size; i++) {
+    for (int i = 0; i < size; i++) 
+    {
         count[arr[i]]++;
     }
 
     // Записуємо відсортовані елементи назад в початковий масив
     int index = 0;
-    for (int i = 0; i <= maxElement; i++) {
-        while (count[i] > 0) {
+    for (int i = 0; i <= maxElement; i++) 
+    {
+        while (count[i] > 0) 
+        {
             arr[index++] = i;
             count[i]--;
         }
@@ -29,25 +61,4 @@ void countingSort(int arr[], int size) {
 
     // Звільняємо пам'ять
     delete[] count;
-}
-
-int main() {
-    int arr[] = { 4, 2, 2, 8, 3, 3, 1 };
-    int size = sizeof(arr) / sizeof(arr[0]);  // Розмір масиву
-
-    cout << "Початковий масив: ";
-    for (int i = 0; i < size; i++) {
-        cout << arr[i] << " ";
-    }
-    cout << endl;
-
-    countingSort(arr, size);  // Викликаємо функцію сортування
-
-    cout << "Відсортований масив: ";
-    for (int i = 0; i < size; i++) {
-        cout << arr[i] << " ";
-    }
-    cout << endl;
-
-    return 0;
 }
