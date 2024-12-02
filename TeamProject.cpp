8e076251106eb6d98f028b335b8c61ebd18d8714
Admin <126456347+Vlad123W@users.noreply.github.com>
2024-12-02 14:14:26 +0200

    /

--- TeamProject.cpp
+++ TeamProject.cpp
@@ -1,122 +1,7 @@
-﻿// Члени команди:
-// - Дужак І.В.
-// - Лобанов Д.С.
-// - Максимов Д.В.
-// - Конопленко В.О.
-// - Мозговий А.А.
-// Назва дисципліни: Алгоритми та структури даих
-// Назва завдання: код\реалізація основної програми з коментарями 
-// Кінцевий термін: 03.12
-// Час виконання: 9 годин
-// Ця робота зроблена членами команди №2
-#include <iostream>
-#include <iomanip>
-
-using namespace std;
-
-// прототип функції сортування
-void countingSortAndAnalyze(int arr[], int size, int& maxElement, int*& countArray);
-
-// головна функція
-int main()
+﻿void countingSort(int arr[], int size)
 {
-    // запит кількості опитуваних
-    int numRespondents;
-    cout << "---==== SURVEY ANALYSIS ====---" << endl;
-    cout << "Enter the number of respondents: ";
-    cin >> numRespondents;
-
-    while (numRespondents <= 0)
-    {
-        cout << "The number of respondents must be greater than 0." << endl;
-        cout << "Enter the number of respondents: ";
-        cin >> numRespondents;
-    }
-
-    // створення динамічного масиву для зберігання відповідей
-    int* responses = new int[numRespondents];
-    cout << "Enter the responses (integer values):" << endl;
-    for (int i = 0; i < numRespondents; i++)
-    {
-        cin >> responses[i];
-    }
-
-    // змінні для максимального варіанту та масиву підрахунку
-    int maxElement = 0;
-    int* countArray = nullptr;
-
-    // виконання сортування та аналізу
-    countingSortAndAnalyze(responses, numRespondents, maxElement, countArray);
-
-    // вивід відсортованих результатів
-    cout << "----------------------" << endl;
-    cout << "Sorted responses: ";
-    for (int i = 0; i < numRespondents; i++)
-    {
-        cout << responses[i] << " ";
-    }
-    cout << endl;
-
-    // вивід кількості за варіантами
-    cout << "----------------------" << endl;
-    cout << "Count of responses by option:" << endl;
-    for (int i = 0; i <= maxElement; i++)
-    {
-        if (countArray[i] > 0)
-        {
-            cout << "Option " << i << ": " << countArray[i] << endl;
-        }
-    }
-
-    // вивід результатів у відсотках
-    cout << "----------------------" << endl;
-    cout << "Percentage results by option:" << endl;
-    for (int i = 0; i <= maxElement; i++)
-    {
-        if (countArray[i] > 0)
-        {
-            double percentage = (static_cast<double>(countArray[i]) / numRespondents) * 100;
-            cout << "Option " << i << ": " << fixed << setprecision(2) << percentage << "%" << endl;
-        }
-    }
-
-    // вивід мінімального і максимального варіантів відповідей
-    int maxOption = -1, minOption = -1;
-    int maxCount = 0, minCount = numRespondents + 1;
-
-    for (int i = 0; i <= maxElement; i++)
-    {
-        if (countArray[i] > 0)
-        {
-            if (countArray[i] > maxCount)
-            {
-                maxCount = countArray[i];
-                maxOption = i;
-            }
-            if (countArray[i] < minCount)
-            {
-                minCount = countArray[i];
-                minOption = i;
-            }
-        }
-    }
-    cout << "----------------------" << endl;
-    cout << "Option with the highest count: Option " << maxOption << " (" << maxCount << " responses)" << endl;
-    cout << "Option with the lowest count: Option " << minOption << " (" << minCount << " responses)" << endl;
-    cout << "----------------------" << endl;
-
-    // Звільнення пам'яті
-    delete[] responses;
-    delete[] countArray;
-
-    return 0;
-}
-
-// функція для сортування методом підрахунку
-void countingSortAndAnalyze(int arr[], int size, int& maxElement, int*& countArray)
-{
-    // знаходження максимального елементу
-    maxElement = arr[0];
+    // Знаходимо максимальний елемент
+    int maxElement = arr[0];
     for (int i = 1; i < size; i++)
     {
         if (arr[i] > maxElement)
@@ -125,29 +10,26 @@ void countingSortAndAnalyze(int arr[], int size, int& maxElement, int*& countArr
         }
     }
 
-    // створення динамічного масиву для підрахунку елементів
-    countArray = new int[maxElement + 1]();
-    int* tempCountArray = new int[maxElement + 1]();
+    // Створюємо динамічний масив для підрахунку елементів
+    int* count = new int[maxElement + 1]();  // Ініціалізуємо масив з нулями
 
-    // підрахунок кількості кожного елемента
+    // Підраховуємо кількість кожного елемента
     for (int i = 0; i < size; i++)
     {
-        countArray[arr[i]]++;
-        tempCountArray[arr[i]]++;
-
+        count[arr[i]]++;
     }
 
-    // записуємо відсортованого елемента назад в початковий масив
+    // Записуємо відсортовані елементи назад в початковий масив
     int index = 0;
     for (int i = 0; i <= maxElement; i++)
     {
-        while (tempCountArray[i] > 0)
+        while (count[i] > 0)
         {
             arr[index++] = i;
-            tempCountArray[i]--;
+            count[i]--;
         }
     }
 
-    // звільнення пам'яті тимчасового масиву
-    delete[] tempCountArray;
+    // Звільняємо пам'ять
+    delete[] count;
 }
